5e729bbfa35cf7f08232df8ba3aae447a492ff3c
Nikolay <Horushko.Nikolay@gmail.com>
2015-06-12 17:46:10 +0300

    add name and description

---
+++ src/main/java/ru/my/mavenproject/Main.java
@@ -0,0 +1,10 @@
+package ru.my.mavenproject;
+
+/**
+ * Created by Nikolay on 11.06.2015.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.format("Hello Maven World");
+    }
+}
