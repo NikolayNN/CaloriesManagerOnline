93ed4d64f97ddc96cab6ad9d4d0b284cc30d98ff
Nikolay <Horushko.Nikolay@gmail.com>
2015-06-15 20:44:19 +0300

    add and implement web.xml

---
+++ src/main/java/ru/my/mavenproject/web/UserServlet.java
@@ -0,0 +1,16 @@
+package ru.my.mavenproject.web;
+
+import java.io.IOException;
+
+/**
+ * Created by Nikolay on 15.06.2015.
+ */
+public class UserServlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+}
