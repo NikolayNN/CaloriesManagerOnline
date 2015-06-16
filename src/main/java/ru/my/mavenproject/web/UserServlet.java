76b6dd6e97455c3e3d60719d618cd34e48443476
Nikolay <Horushko.Nikolay@gmail.com>
2015-06-17 00:57:28 +0300

    hello tomcat

--- src/main/java/ru/my/mavenproject/web/UserServlet.java
+++ src/main/java/ru/my/mavenproject/web/UserServlet.java
@@ -1,16 +1,21 @@
 package ru.my.mavenproject.web;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 /**
  * Created by Nikolay on 15.06.2015.
  */
-public class UserServlet extends javax.servlet.http.HttpServlet {
-    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+public class UserServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
     }
 
-    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+        request.getRequestDispatcher("/userList.jsp").forward(request,response);
     }
 }
